ad604693fa69c4b82b501495e8aac6bc88ff4aca
swarupyeole <swarupyeole11@gmail.com>
2023-05-06 17:27:19 +0530

    Added the work experience of Newton School

--- src/constants/index.js
+++ src/constants/index.js
@@ -166,10 +166,10 @@ const experiences = [
     iconBg: "#E6DEDD",
     date: "Jan 2023 - Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Create and review Real-life programming questions and test problems created by other problem setters along with their test cases for frontend technologies (HTML, CSS, JS & React)",
+      "Created assessment-based multiple choice questions for learning outcomes for the frontend technologies (HTML, CSS, JS & React).",
+      "Implemented and ensured best practices are followed in all technical content also performed testing using cypress.",
+      "Work closely with the team to ensure we help enable students to have the best full-stack talent.",
     ],
   },
 ];
