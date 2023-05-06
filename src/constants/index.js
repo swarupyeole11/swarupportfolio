45987421ef58f9a8ad35edbeab4fd8ebd4c8e73f
swarupyeole <swarupyeole11@gmail.com>
2023-05-06 17:39:31 +0530

    Added the work experinece of kotak mahindra

--- src/constants/index.js
+++ src/constants/index.js
@@ -140,10 +140,9 @@ const experiences = [
     iconBg: "#E6DEDD",
     date: "May 2023 - July 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Performing Data Analysis on the available data to provide useful insights.",
+      "Collaborating with cross-functional teams including Data Analytics , product managers, and other interns.",
+      "Developing Machine Learning Model to predict whether to provide loans to the customer or not .",
     ],
   },
   {
